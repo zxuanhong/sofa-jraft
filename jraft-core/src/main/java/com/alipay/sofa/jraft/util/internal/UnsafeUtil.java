25ff077c2e4f37c4233d20927608a7e4c423fbfb
xuanhong <62740686+zxuanhong@users.noreply.github.com>
2024-12-22 17:32:24 +0800

    feat: 重构

--- jraft-core/src/main/java/com/alipay/sofa/jraft/util/internal/UnsafeUtil.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/util/internal/UnsafeUtil.java
@@ -19,8 +19,6 @@ package com.alipay.sofa.jraft.util.internal;
 import java.lang.reflect.Field;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- jraft-core/src/main/java/com/alipay/sofa/jraft/util/internal/Updaters.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/util/internal/Updaters.java
@@ -29,43 +29,6 @@ public class Updaters {
      * @param tClass    the class of the objects holding the field.
      * @param fieldName the name of the field to be updated.
      */
-    public static <U> IntegerFieldUpdater<U> newIntegerFieldUpdater(final Class<? super U> tClass,
-                                                                    final String fieldName) {
-        try {
-            if (UnsafeUtil.hasUnsafe()) {
-                return new UnsafeIntegerFieldUpdater<>(UnsafeUtil.getUnsafeAccessor().getUnsafe(), tClass, fieldName);
-            } else {
-                return new ReflectionIntegerFieldUpdater<>(tClass, fieldName);
-            }
-        } catch (final Throwable t) {
-            throw new RuntimeException(t);
-        }
-    }
-
-    /**
-     * Creates and returns an updater for objects with the given field.
-     *
-     * @param tClass    the class of the objects holding the field.
-     * @param fieldName the name of the field to be updated.
-     */
-    public static <U> LongFieldUpdater<U> newLongFieldUpdater(final Class<? super U> tClass, final String fieldName) {
-        try {
-            if (UnsafeUtil.hasUnsafe()) {
-                return new UnsafeLongFieldUpdater<>(UnsafeUtil.getUnsafeAccessor().getUnsafe(), tClass, fieldName);
-            } else {
-                return new ReflectionLongFieldUpdater<>(tClass, fieldName);
-            }
-        } catch (final Throwable t) {
-            throw new RuntimeException(t);
-        }
-    }
-
-    /**
-     * Creates and returns an updater for objects with the given field.
-     *
-     * @param tClass    the class of the objects holding the field.
-     * @param fieldName the name of the field to be updated.
-     */
     public static <U, W> ReferenceFieldUpdater<U, W> newReferenceFieldUpdater(final Class<? super U> tClass,
                                                                               final String fieldName) {
         try {
